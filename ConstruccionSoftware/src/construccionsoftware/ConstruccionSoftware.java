222b4c84a311a42e611abc4ef30a78bf9de801cb
JohanRomeroM <johan.romero@netapplications.com.co>
2020-02-03 13:32:35 -0500

    Interface

--- ConstruccionSoftware/src/construccionsoftware/ConstruccionSoftware.java
+++ ConstruccionSoftware/src/construccionsoftware/ConstruccionSoftware.java
@@ -27,6 +27,25 @@ public class ConstruccionSoftware {
         //se muestran los datos de la persona en un mensaje.-
         JOptionPane.showMessageDialog(null,"Nombre Completo:\n"+ p.datos()+"\n"
                 + "Edad: "+p.edad()+" años");
+        
+        // creating an inatance of Bicycle  
+        // doing some operations  
+        Bicycle bicycle = new Bicycle(); 
+        bicycle.changeGear(2); 
+        bicycle.speedUp(3); 
+        bicycle.applyBrakes(1); 
+          
+        System.out.println("Bicycle present state :"); 
+        bicycle.printStates(); 
+          
+        // creating instance of the bike. 
+        Bike bike = new Bike(); 
+        bike.changeGear(1); 
+        bike.speedUp(4); 
+        bike.applyBrakes(3); 
+          
+        System.out.println("Bike present state :"); 
+        bike.printStates(); 
+    } 
     }
     
-}
